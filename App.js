abb5736ccddb2d51a9546012d87775378f9d2c09
Abhishek.kumar3 <Abhishek.kumar3@businessnext.com>
2023-07-09 02:07:34 +0530

    assignment2 is going on

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "child1"}, "hello h1 tag"), React.createElement("h2", {id: "child2"}, "hello h2 tag")]));
+import React from 'react'
+import  ReactDOM  from 'react-dom/client';
+
+const heading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "child1"}, "hello abhishe tag"), React.createElement("h2", {id: "child2"}, "hello h2 tag")]));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
---
+++ README.md
@@ -0,0 +1,22 @@
+# Namaste React
+
+# parcel
+
+- Dev Build
+- Local Server
+- HMP = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Build
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- consistent Hashing
+- code Splitting
+- Differential bundling - support older browser
+
+
+# npx parcel build index.html -> this is used for production build
+# npx parcel index.html ->  is used for development build
+
+
