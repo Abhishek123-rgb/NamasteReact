0a8cc9341f60f22b56b60ef1246ddd9ecb6f8fe0
Abhishek.kumar3 <Abhishek.kumar3@businessnext.com>
2023-07-17 22:07:39 +0530

    sorting done for listofrestaurant for assignment4

--- components/Body.js
+++ components/Body.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import {Restrocard} from "./Restrocard";
 import { restaurantList } from "./utils/constant";
+
 const Body = () => {
+
+  const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+
+  const ratedRestaurant = () => {
+    const filteredList =  listOfRestaurant.filter((item) => item?.data?.avgRating > 4);
+    setListOfRestaurant(filteredList)
+  }
+
   return (
     <div>
       <div className="search"> Search </div>
+      <button onClick={ratedRestaurant}>Top Rated Restaurant</button>
       <div className="res-container">
-        {restaurantList &&
-          restaurantList.map((item) => {
+        {listOfRestaurant &&
+          listOfRestaurant.map((item) => {
             return <Restrocard data={item} />;
           })}
       </div>
