5f9a54483a56f96416b7e5ff0f92c934e0153d68
Abhishek.kumar3 <Abhishek.kumar3@businessnext.com>
2023-07-20 15:32:31 +0530

    search food done

--- components/Body.js
+++ components/Body.js
@@ -1,24 +1,37 @@
-import { useState } from "react";
-import {Restrocard} from "./Restrocard";
+import { useEffect, useState } from "react";
+import { Restrocard } from "./Restrocard";
 import { restaurantList } from "./utils/constant";
+import axios from "axios";
 
 const Body = () => {
-
   const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+  const [searchlist, setSearchlist] = useState("");
+  const [filterlist, setFilterList] = useState(restaurantList);
 
   const ratedRestaurant = () => {
-    const filteredList =  listOfRestaurant.filter((item) => item?.data?.avgRating > 3.9);
-    setListOfRestaurant(filteredList)
+    const filteredList = listOfRestaurant.filter(
+      (item) => item?.data?.avgRating > 3.9
+    );
+    setListOfRestaurant(filteredList);
+  };
+
+  const handleSerach = () => {
+    const filterdata = listOfRestaurant.filter((item) => item?.data?.name.toLowerCase().includes(searchlist));
+    setFilterList(filterdata)
   }
 
-  lmjcsjkd
   return (
     <div>
-      <div className="search"> Search </div>
-      <button onClick={ratedRestaurant}>Top Rated Restaurant</button>
+      <div className="header1">
+        <div className="search">
+          <input type="text" value={searchlist} onChange={(e) => setSearchlist(e.target.value)} />
+          <button style={{marginLeft: "5px"}} onClick={handleSerach}>Search</button>
+        </div>
+        <button className="toprated" onClick={ratedRestaurant}>Top Rated Restaurant</button>
+      </div>
       <div className="res-container">
-        {listOfRestaurant &&
-          listOfRestaurant.map((item) => {
+        {filterlist &&
+          filterlist.map((item) => {
             return <Restrocard data={item} />;
           })}
       </div>
